66ed3b9c39b3646154cc8d08a4486af1d35b5a31
Boyd Belshof <boyd096@gmail.com>
2015-05-26 13:07:43 +0200

    hallo

--- TwitterClient/src/nl/northcreek/twitazia/model/Contributor.java
+++
@@ -1,7 +0,0 @@
-package nl.northcreek.twitazia.model;
-
-public class Contributor {
-	private long id;
-	private String id_str;
-	private String screen_name;
-}
--- TwitterClient/src/nl/northcreek/twitazia/model/Place.java
+++
@@ -1,5 +0,0 @@
-package nl.northcreek.twitazia.model;
-
-public class Place {
-
-}
